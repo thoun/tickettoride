6ec448d6f74d3d391700854959aae6aad7838a1f
Guy Baudouin <guythoun@gmail.com>
2021-11-04 12:17:19 +0100

    init referential

--- material.inc.php
+++ material.inc.php
@@ -19,8 +19,70 @@
  *
  */
 
+ /*
+Atlanta
+Boston
+Calgary
+Chicago
+Dallas
+Denver
+Duluth
+El Paso
+Helena
+Houston
+Kansas City
+Little Rock
+Los Angeles
+Miami
+Montréal
+Nashville
+New Orleans
+New York
+Oklahoma City
+Phoenix
+Pittsburgh
+Portland
+Salt Lake City
+Sault St. Marie
+Santa Fe
+Seattle
+Toronto
+Vancouver
+Winnipeg*/
 
 /*
+From	To	Points
+Boston	Miami	12
+Calgary	Phoenix	13
+Calgary	Salt Lake City	7
+Chicago	New Orleans	7
+Chicago	Santa Fe	9
+Dallas	New York	11
+Denver	El Paso	4
+Denver	Pittsburgh	11
+Duluth	El Paso	10
+Duluth	Houston	8
+Helena	Los Angeles	8
+Kansas City	Houston	5
+Los Angeles	Chicago	16
+Los Angeles	Miami	20
+Los Angeles	New York	21
+Montréal	Atlanta	9
+Montréal	New Orleans	13
+New York	Atlanta	6
+Portland	Nashville	17
+Portland	Phoenix	11
+San Francisco	Atlanta	17
+Sault St. Marie	Nashville	8
+Sault St. Marie	Oklahoma City	9
+Seattle	Los Angeles	9
+Seattle	New York	22
+Toronto	Miami	10
+Vancouver	Montréal	20
+Vancouver	Santa Fe	13
+Winnipeg	Houston	12
+Winnipeg	Little Rock	11
+
 
 Example:
 
