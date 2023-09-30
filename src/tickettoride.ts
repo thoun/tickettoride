a66ea0d657b17f1e6745066cc2421ef98baa2d13
Guy Baudouin <guythoun@gmail.com>
2023-09-30 16:35:43 +0200

    SWITCH TO TTRE PROJECT

--- src/tickettoride.ts
+++ src/tickettoride.ts
@@ -866,7 +866,7 @@ class TicketToRide implements TicketToRideGame {
     public takeAction(action: string, data?: any) {
         data = data || {};
         data.lock = true;
-        (this as any).ajaxcall(`/tickettoride/tickettoride/${action}.html`, data, this, () => {});
+        (this as any).ajaxcall(`/tickettorideeurope/tickettorideeurope/${action}.html`, data, this, () => {});
     }
 
     private isFastEndScoring() {
