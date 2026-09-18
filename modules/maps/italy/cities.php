7dca5f5972108aa32cb52852be5e188c6f210135
Guy <guy@boardgamearena.com>
2026-09-18 16:40:16 +0200

    MC7 map img

---
+++ modules/maps/italy/cities.php
@@ -0,0 +1,12 @@
+<?php
+
+use Bga\Games\TicketToRide\Objects\City;
+
+/**
+ * Cities in the map (by alphabetical order).
+ */
+function getCities() {
+  return [
+  ];
+}
+                                 
---
+++ modules/maps/italy/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+use Bga\Games\TicketToRide\Objects\Route;
+use Bga\Games\TicketToRide\Objects\RouteSpace;
+
+/**
+ * Route on the map. 
+ * For double routes, there is 2 instances of Route.
+ * For cities (from/to), it's always low id to high id.
+ */
+function getRoutes() {
+  return [
+  ];
+}
+
